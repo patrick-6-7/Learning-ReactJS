f3936d66c47a17f688c6217229cef0541f248b47
Prateek <prateekprajapati062004@gmail.com>
2025-11-05 12:43:44 +0530

    intigrated tailwind css using tailwind + vit installation

---
+++ 1_experiments/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './Button.jsx'
+import Card from './Card.jsx'
+
+export default function App() {
+  return (
+    <div class="flex flex-wrap justify-center">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
---
+++ 1_experiments/src/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button() {	
+  return (
+    <button class="bg-blue-900 rounded-md size-100 text-white">
+      Click Me
+    </button>
+  );
+}
